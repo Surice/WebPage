bd3571e4956b666d4dcf573475753ec2eab8442a
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-09-20 11:35:29 +0200

    dev header mobile optimized using dropdown

--- src/frontEnd/dev/header.php
+++ src/frontEnd/dev/header.php
@@ -3,16 +3,35 @@
         <link rel="stylesheet" id="style">
     </head>
     <body>
-    <ul class="navbar-me">
-        <li class="li"><a class="nav-a" id="home">Home</a></li>
-        <li class="li"><a class="nav-a" id="visits">Visitors</a></li>
-        <li class="li"><a class="nav-a" id="iplog">Log</a></li>
-        <li class="li"><a class="nav-a" id="cloud">Cloud</a></li>
-        <li class="li"><a class="nav-a" id="ipcam">IP Cam</a></li>
-        <li class="li"><a class="nav-a" href="">None</a></li>
-        <li class="li"><a href="./login.php?action=logout"><button class="logout btn btn-outline-danger">Logout</button></a></li>
-        <li class="li"><code class="logDat">Logged in as: <?php echo $_SESSION["user"] ?></code></li>
-    </ul>
+    <div class="navbar-me">
+        <ul class="nav-content">
+            <a class="nav-a" id="home">Home</a>
+            <a class="nav-a" id="visits">Visitors</a>
+            <a class="nav-a" id="iplog">Log</a>
+            <a class="nav-a" id="cloud">Cloud</a>
+            <a class="nav-a" id="ipcam">IP Cam</a>
+
+            <a href="./login.php?action=logout"><button class="logout btn btn-outline-danger">Logout</button></a>
+            <code class="logDat">Logged in as: <?php echo $_SESSION["user"] ?></code>
+        </ul>
+        <div class="dropdown">
+            <button class="dropbtn">Menu ↓</button>
+
+            <a class="unused" href="./login.php?action=logout"><button class="logout btn btn-outline-danger">Logout</button></a>
+            <code class="logDat">Logged in as: <?php echo $_SESSION["user"] ?></code>
+
+            <div class="dropdown-content">
+                <a class="nav-a" id="homeDrop">Home</a>
+                <a class="nav-a" id="visitsDrop">Visitors</a>
+                <a class="nav-a" id="iplogDrop">Log</a>
+                <a class="nav-a" id="cloudDrop">Cloud</a>
+                <a class="nav-a" id="ipcamDrop">IP Cam</a>
+            </div>
+        </div>
+
+
+    </div>
+
     </body>
     <script>
         var path = window.location.href.split('/');
@@ -22,9 +41,14 @@
         document.getElementById('style').href = `${path}/styleHeader.css`;
 
         document.getElementById('home').href = `${path}/index.php`;
+        document.getElementById('homeDrop').href = `${path}/index.php`;
         document.getElementById('visits').href = `${path}/userInfo/index.php`;
+        document.getElementById('visitsDrop').href = `${path}/userInfo/index.php`;
         document.getElementById('iplog').href = `${path}/ipLog/index.php`;
+        document.getElementById('iplogDrop').href = `${path}/ipLog/index.php`;
         document.getElementById('cloud').href = `${path}/cloud/index.php`;
+        document.getElementById('cloudDrop').href = `${path}/cloud/index.php`;
         document.getElementById('ipcam').href = `${path}/ipCam/index.php`;
+        document.getElementById('ipcamDrop').href = `${path}/ipCam/index.php`;
     </script>
 </html>
